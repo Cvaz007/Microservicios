8a4e63c752115594061eafb278abf59459a86214
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2024-04-24 07:11:48 -0500

    first implementation

--- 01 - SimpleMicroservice/qualification/src/main/java/com/microservice/qualification/QualificationApplication.java
+++ 01 - SimpleMicroservice/qualification/src/main/java/com/microservice/qualification/QualificationApplication.java
@@ -2,8 +2,10 @@ package com.microservice.qualification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class QualificationApplication {
 
 	public static void main(String[] args) {

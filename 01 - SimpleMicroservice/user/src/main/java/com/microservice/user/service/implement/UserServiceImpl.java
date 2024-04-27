531273da37aabd4e853ced2c46dfdedd032cd398
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2024-04-26 19:56:24 -0500

    connet microseervices advance

--- 01 - SimpleMicroservice/user/src/main/java/com/microservice/user/service/implement/UserServiceImpl.java
+++ 01 - SimpleMicroservice/user/src/main/java/com/microservice/user/service/implement/UserServiceImpl.java
@@ -2,12 +2,20 @@ package com.microservice.user.service.implement;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservice.user.entity.UserEntity;
 import com.microservice.user.exception.ResourceNotFoundException;
+import com.microservice.user.external.HotelService;
+import com.microservice.user.model.Hotel;
+import com.microservice.user.model.Qualification;
 import com.microservice.user.repository.UserRepository;
 import com.microservice.user.service.UserService;
 
@@ -15,7 +23,15 @@ import com.microservice.user.service.UserService;
 public class UserServiceImpl implements UserService {
 
   @Autowired
+  private RestTemplate restTemplate;
+
+  @Autowired
+  private HotelService hotelService;
+
+  @Autowired
   private UserRepository userRepository;
+
+  private Logger logger = LoggerFactory.getLogger(UserService.class);
 
   @Override
   public List<UserEntity> getAllUsers() {
@@ -26,8 +42,26 @@ public class UserServiceImpl implements UserService {
   @Override
   public UserEntity getUser(String userId) {
 
-    return userRepository.findById(userId)
+    UserEntity user = userRepository.findById(userId)
         .orElseThrow(() -> new ResourceNotFoundException("User not fount with the ID: " + userId));
+
+    Qualification[] userQualification = restTemplate
+        .getForObject("http://CALIFICACION-SERVICE/calificaciones/usuarios/" + user.getUserId(), Qualification[].class);
+    logger.info("{}", userQualification);
+
+    List<Qualification> qualifications = Arrays.stream(userQualification).collect(Collectors.toList());
+
+    List<Qualification> qualificationsList = qualifications.stream().map(qualification -> {
+      System.out.println(qualification.getHotelId());
+
+      Hotel hotel = hotelService.getHotel(qualification.getHotelId());
+
+      qualification.setHotel(hotel);
+      return qualification;
+    }).collect(Collectors.toList());
+
+    user.setQualifications(qualificationsList);
+    return user;
   }
 
   @Override
